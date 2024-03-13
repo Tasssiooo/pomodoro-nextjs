c6bec64857f4bcfe141e588db8724b50ec462af3
Tasssiooo <tassioribeirofontes@gmail.com>
2024-03-13 00:13:58 -0300

    Env variables test

--- db/pool.ts
+++ db/pool.ts
@@ -1,11 +1,11 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT as any,
-  database: process.env.DB,
+  user: process.env.NEXT_PUBLIC_DB_USER,
+  password: process.env.NEXT_PUBLIC_DB_PASSWORD,
+  host: process.env.NEXT_PUBLIC_DB_HOST,
+  port: process.env.NEXT_PUBLIC_DB_PORT as any,
+  database: process.env.NEXT_PUBLIC_DB,
 });
 
 export default pool;
